21bcd02e161cd6418286693325ecfab814446240
makiasake <alexandre.maki@hotmail.com>
2023-06-14 11:16:14 -0300

    Updated readme and simplifed test class

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # tess4j-test-java
+
+### Project created to test ocr recognition with Tess4J
+
+### References
+1. [Tess4J](https://tess4j.sourceforge.net/)
+2. [Tesseract](https://github.com/tesseract-ocr/tesseract)
+3. [Trained Data](https://github.com/tesseract-ocr/tessdata)
+
+### Setup
+
+Create two folders: src/main/resources/tessdata and src/main/resources/test-files/
+1. src/main/resources/tessdata: will contain the trained models for wich language your test files will be (more on [Trained Data](https://github.com/tesseract-ocr/tessdata))
+2. src/main/resources/test-files: Is where you will place the test files
+
+To run a test just run TestTess4J class changing the file and language parameters.
--- src/test/java/tess4j/java/test/TestTess4J.java
+++ src/test/java/tess4j/java/test/TestTess4J.java
@@ -2,7 +2,6 @@ package tess4j.java.test;
 
 import java.io.File;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import net.sourceforge.tess4j.Tesseract;
@@ -11,8 +10,7 @@ import net.sourceforge.tess4j.TesseractException;
 public class TestTess4J {
 
 	@Test
-	@Disabled
-	public void testPng() {
+	public void test() {
 
 		File image = new File("src/main/resources/test-files/1686745622599.png");
 		Tesseract tesseract = new Tesseract();
@@ -23,42 +21,6 @@ public class TestTess4J {
 
 		try {
 			String result = tesseract.doOCR(image);
-			System.out.println(result);
-		} catch (TesseractException ex) {
-			System.out.println("Error: " + ex.getMessage());
-		}
-	}
-
-	@Test
-	public void testPdf() {
-
-		File file = new File("src/main/resources/test-files/31230614055516000814550700000733281934404858.pdf");
-		Tesseract tesseract = new Tesseract();
-		tesseract.setDatapath("src/main/resources/tessdata");
-		tesseract.setLanguage("por");
-		tesseract.setPageSegMode(1);
-		tesseract.setOcrEngineMode(1);
-
-		try {
-			String result = tesseract.doOCR(file);
-			System.out.println(result);
-		} catch (TesseractException ex) {
-			System.out.println("Error: " + ex.getMessage());
-		}
-	}
-
-	@Test
-	@Disabled
-	public void testManuscript() {
-		File file = new File("src/main/resources/test-files/1686747603382.png");
-		Tesseract tesseract = new Tesseract();
-		tesseract.setDatapath("src/main/resources/tessdata");
-		tesseract.setLanguage("por");
-		tesseract.setPageSegMode(1);
-		tesseract.setOcrEngineMode(1);
-
-		try {
-			String result = tesseract.doOCR(file);
 			System.out.println(result);
 		} catch (TesseractException ex) {
 			System.out.println("Error: " + ex.getMessage());
